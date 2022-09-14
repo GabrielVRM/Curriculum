ddfd1d77d0c3523cb93eb035d325fee6236ce389
GabrielVRM <Gabriel.vieira2595@gmail.com>
2022-09-14 11:53:07 -0300

    att

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,13 +11,15 @@ export function Contact() {
         <div className=" w-10 h-[0.5px] m-4 border bg-white" />
       </div>
         <div className="py-20 grid grid-cols-1 text-center">
+        <span>São Paulo - SP </span>
+          <span>Rua Geraldo do Brumado 102 - Grajaú</span>
+          <br/>
           <a href="mailto:gabriel.vieira2595@gmail.com">
             Gabriel.Vieira2595@gmail.com 
           </a>
           <a href="https://wa.me/5511986431855">
             (55) 11 9 8643-1855
           </a>
-          
         </div>
          
     </div>
