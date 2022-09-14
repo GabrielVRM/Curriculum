fea33f8d694ff50dda8d5ebbaa69fb267ca6de75
GabrielVRM <Gabriel.vieira2595@gmail.com>
2022-09-14 11:35:39 -0300

    att

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://gabrielvrm.github.io/Presentation/',
+  base: 'https://gabrielvrm.github.io/Dev-web/',
 
   plugins: [react()]
 })
